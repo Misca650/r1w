0ad18b19f6231634bd0b474ed1679268014ad6b6
Misca650 <miateimmteduk@gmail.com>
2026-06-11 01:18:30 +0700

    Create user.js

---
+++ js/config/user.js
@@ -0,0 +1,127 @@
+export const config = {
+
+    conutMEMBERS: 16,
+
+    allUsers: {
+        THEBOSS: [
+            { 
+                name: "Cham Svenxbzy", 
+                image: "https://cdn.discordapp.com/attachments/1430828076023021568/1486639536548347994/ChatGPT_Image_Mar_26_2026_03_14_18_PM.png?ex=6a2a6e75&is=6a291cf5&hm=aea3420e7873d6f35de608ab1f6ca62443c4b7f3e77d19d4e055e614eea106ef&", 
+                instagramURL: "https://discord.gg/rank1", 
+                color: "#ff9100ff", 
+                icon: "fas fa-crown" 
+            },
+            { 
+                name: "J4ys1x", 
+                image: "https://cdn.discordapp.com/attachments/1430828076023021568/1486639536548347994/ChatGPT_Image_Mar_26_2026_03_14_18_PM.png?ex=6a2a6e75&is=6a291cf5&hm=aea3420e7873d6f35de608ab1f6ca62443c4b7f3e77d19d4e055e614eea106ef&", 
+                instagramURL: "https://discord.gg/rank1", 
+                color: "#ff9100ff", 
+                icon: "fas fa-crown" 
+            },
+            { 
+                name: "Taka Rexel", 
+                image: "https://cdn.discordapp.com/attachments/1430828076023021568/1486639536548347994/ChatGPT_Image_Mar_26_2026_03_14_18_PM.png?ex=6a2a6e75&is=6a291cf5&hm=aea3420e7873d6f35de608ab1f6ca62443c4b7f3e77d19d4e055e614eea106ef&", 
+                instagramURL: "https://discord.gg/rank1", 
+                color: "#ff9100ff", 
+                icon: "fas fa-crown" 
+            },
+        ],
+
+        ADMIN: [
+            { 
+                name: "𝓗𝓽𝓶𝓵𝓧𝓓𝓔𝓥 ⚔", 
+                image: "https://cdn.discordapp.com/attachments/1430828076023021568/1486639536548347994/ChatGPT_Image_Mar_26_2026_03_14_18_PM.png?ex=6a2a6e75&is=6a291cf5&hm=aea3420e7873d6f35de608ab1f6ca62443c4b7f3e77d19d4e055e614eea106ef&", 
+                instagramURL: "󠁛󠀣󠀱󠁤󠀰󠀰󠀷󠀰󠀬󠀣󠀰󠀹󠀰󠀰󠀱󠁤󠁝https://www.instagram.com/nullstudio.dll/", 
+                color: "#ff0000ff", 
+                icon: "fas fa-crown" 
+            },
+        ],
+
+        สมาชิก: [
+            { 
+                name: "Dino Vajen  (@eoeonhocc)", 
+                image: "https://cdn.discordapp.com/attachments/1430828076023021568/1486639536548347994/ChatGPT_Image_Mar_26_2026_03_14_18_PM.png?ex=6a2a6e75&is=6a291cf5&hm=aea3420e7873d6f35de608ab1f6ca62443c4b7f3e77d19d4e055e614eea106ef&", 
+                instagramURL: "https://discord.gg/rank1", 
+                color: "#ffffffff", 
+                icon: "fas fa-user" 
+            },
+            { 
+                name: "Rafael Indazy  (@Gayanleza3626) ", 
+                image: "https://cdn.discordapp.com/attachments/1430828076023021568/1486639536548347994/ChatGPT_Image_Mar_26_2026_03_14_18_PM.png?ex=6a2a6e75&is=6a291cf5&hm=aea3420e7873d6f35de608ab1f6ca62443c4b7f3e77d19d4e055e614eea106ef&", 
+                instagramURL: "https://discord.gg/rank1", 
+                color: "#ffffffff", 
+                icon: "fas fa-user" 
+            },
+            { 
+                name: "Frt PERFECTDARA   (@huiyikkgguin)", 
+                image: "https://cdn.discordapp.com/attachments/1430828076023021568/1486639536548347994/ChatGPT_Image_Mar_26_2026_03_14_18_PM.png?ex=6a2a6e75&is=6a291cf5&hm=aea3420e7873d6f35de608ab1f6ca62443c4b7f3e77d19d4e055e614eea106ef&", 
+                instagramURL: "https://discord.gg/rank1", 
+                color: "#ffffffff", 
+                icon: "fas fa-user" 
+            },
+            { 
+                name: "P Sohot  (@PaPae2009i)", 
+                image: "https://cdn.discordapp.com/attachments/1430828076023021568/1486639536548347994/ChatGPT_Image_Mar_26_2026_03_14_18_PM.png?ex=6a2a6e75&is=6a291cf5&hm=aea3420e7873d6f35de608ab1f6ca62443c4b7f3e77d19d4e055e614eea106ef&", 
+                instagramURL: "https://discord.gg/rank1", 
+                color: "#ffffffff", 
+                icon: "fas fa-user" 
+            },
+            { 
+                name: "Roti Lemon  (@sofox66407)", 
+                image: "https://cdn.discordapp.com/attachments/1430828076023021568/1486639536548347994/ChatGPT_Image_Mar_26_2026_03_14_18_PM.png?ex=6a2a6e75&is=6a291cf5&hm=aea3420e7873d6f35de608ab1f6ca62443c4b7f3e77d19d4e055e614eea106ef&", 
+                instagramURL: "https://discord.gg/rank1", 
+                color: "#ffffffff", 
+                icon: "fas fa-user" 
+            },
+            { 
+                name: "Real PERFECT (@Agakami_8223)", 
+                image: "https://cdn.discordapp.com/attachments/1430828076023021568/1486639536548347994/ChatGPT_Image_Mar_26_2026_03_14_18_PM.png?ex=6a2a6e75&is=6a291cf5&hm=aea3420e7873d6f35de608ab1f6ca62443c4b7f3e77d19d4e055e614eea106ef&", 
+                instagramURL: "https://discord.gg/rank1", 
+                color: "#ffffffff", 
+                icon: "fas fa-user" 
+            },
+            { 
+                name: "Rain Perfect (@rr_ainx)", 
+                image: "https://cdn.discordapp.com/attachments/1430828076023021568/1486639536548347994/ChatGPT_Image_Mar_26_2026_03_14_18_PM.png?ex=6a2a6e75&is=6a291cf5&hm=aea3420e7873d6f35de608ab1f6ca62443c4b7f3e77d19d4e055e614eea106ef&", 
+                instagramURL: "https://discord.gg/rank1", 
+                color: "#ffffffff", 
+                icon: "fas fa-user" 
+            },
+            { 
+                name: "Arnon King (@Arnon4433)", 
+                image: "https://cdn.discordapp.com/attachments/1430828076023021568/1486639536548347994/ChatGPT_Image_Mar_26_2026_03_14_18_PM.png?ex=6a2a6e75&is=6a291cf5&hm=aea3420e7873d6f35de608ab1f6ca62443c4b7f3e77d19d4e055e614eea106ef&", 
+                instagramURL: "https://discord.gg/rank1", 
+                color: "#ffffffff", 
+                icon: "fas fa-user" 
+            },
+            { 
+                name: "Mano OuttuB (@Gam_Shebada)", 
+                image: "https://cdn.discordapp.com/attachments/1430828076023021568/1486639536548347994/ChatGPT_Image_Mar_26_2026_03_14_18_PM.png?ex=6a2a6e75&is=6a291cf5&hm=aea3420e7873d6f35de608ab1f6ca62443c4b7f3e77d19d4e055e614eea106ef&", 
+                instagramURL: "https://discord.gg/rank1", 
+                color: "#ffffffff", 
+                icon: "fas fa-user" 
+            },
+            { 
+                name: "Jaokun Rankone (@BzDSJnWpuwE)", 
+                image: "https://cdn.discordapp.com/attachments/1430828076023021568/1486639536548347994/ChatGPT_Image_Mar_26_2026_03_14_18_PM.png?ex=6a2a6e75&is=6a291cf5&hm=aea3420e7873d6f35de608ab1f6ca62443c4b7f3e77d19d4e055e614eea106ef&", 
+                instagramURL: "https://discord.gg/rank1", 
+                color: "#ffffffff", 
+                icon: "fas fa-user" 
+            },
+            { 
+                name: "Donthurtmeagain Vex (E_x1q)", 
+                image: "https://cdn.discordapp.com/attachments/1430828076023021568/1486639536548347994/ChatGPT_Image_Mar_26_2026_03_14_18_PM.png?ex=6a2a6e75&is=6a291cf5&hm=aea3420e7873d6f35de608ab1f6ca62443c4b7f3e77d19d4e055e614eea106ef&", 
+                instagramURL: "https://discord.gg/rank1", 
+                color: "#ffffffff", 
+                icon: "fas fa-user" 
+            },
+            { 
+                name: "Wen Forrealflag (ytujgjf_5344 )", 
+                image: "https://cdn.discordapp.com/attachments/1430828076023021568/1486639536548347994/ChatGPT_Image_Mar_26_2026_03_14_18_PM.png?ex=6a2a6e75&is=6a291cf5&hm=aea3420e7873d6f35de608ab1f6ca62443c4b7f3e77d19d4e055e614eea106ef&", 
+                instagramURL: "https://discord.gg/rank1", 
+                color: "#ffffffff", 
+                icon: "fas fa-user" 
+            }
+        ],
+    }
+};
